5afc9ddd2f57f183aba5825fca81d9b606f9e192
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2021-03-21 19:29:40 -0300

    Ejemplo arreglos, for e if

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,8 +2,16 @@ public class Launcher {
 
 	public static void main(String args[]) {
 
-		for (int i = 0; i < 10; i = i + 1) {
-			System.out.println("i = " + i);
+		String a[] = new String[10];
+
+		for (int i = 0; i < 10; i++) {
+
+			if (i % 2 == 0) {
+				a[i] = "El número " + i + " es par";
+			} else {
+				a[i] = "El número " + i + " es impar";
+			}
+			System.out.println(a[i]);
 		}
 
 	}
