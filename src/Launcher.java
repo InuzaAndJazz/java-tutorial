e02f0402656325508873efa879a833336f18194e
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2021-03-21 19:01:27 -0300

    ejemplo ciclos: for

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,10 +2,8 @@ public class Launcher {
 
 	public static void main(String args[]) {
 
-		int a = 0;
-		while (a < 10) {
-			System.out.println("a es menor a 10 por tanto estamos ejecutando lo que está dentro del while");
-			a = a + 1;
+		for (int i = 0; i < 10; i = i + 1) {
+			System.out.println("i = " + i);
 		}
 
 	}
