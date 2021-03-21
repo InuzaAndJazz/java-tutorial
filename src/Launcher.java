2a8129ac5142d523d7f1ae0dbdb15bbb6e35f9bd
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2021-03-21 18:45:22 -0300

    no message

---
+++ src/Launcher.java
@@ -0,0 +1,16 @@
+public class Launcher {
+
+	public static void main(String args[]) {
+
+		int a = 0;
+		if (a > 0) {
+			System.out.println("El número es positivo");
+		} else if (a < 0){
+			System.out.println("El número es negativo");
+		} else {
+			System.out.println("El número es 0");
+		}
+
+	}
+
+}
